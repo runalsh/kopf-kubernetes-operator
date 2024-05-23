165b68ccde2cafe89ba8da00eeb2a3de92b98b4d
runalsh <21105673+runalsh@users.noreply.github.com>
2024-05-23 14:40:19 +0300

    step2

---
+++ helpers/create.py
@@ -0,0 +1,24 @@
+import ...
+
+api = kubernetes_api()
+
+k8s_client = create_k8s_client().CustomObjectsApi()
+core_api = create_k8s_client().CoreV1Api()
+
+def create_service(template):
+    kopf.adopt(template)
+    service = pykube.Service(api, template)
+    service.create()
+    return service
+
+def create_deployment(template):
+    kopf.adopt(template)
+    deployment = pykube.Deployment(api, template)
+    deployment.create()
+    return deployment
+
+def create_service_account(template):
+    kopf.adopt(template)
+    service_account = pykube.ServiceAccount(api, template)
+    service_account.create()
+    return service_account
--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import yaml
 from jinja2 import Environment, FileSystemLoader
 from kopf import ConnectionInfo
 import time
-from helpers.create import create_deployment, create_service, create_
-from helpers.delete import delete_deployment, delete_service, delete_
+from helpers.create import create_deployment, create_service, create_service_account))
+from helpers.delete import delete_deployment, delete_service, delete_service_account))
 
 root_directory = os.path.dirname(os.path.abspath(__file__))
 path = root_directory + "/templates"
@@ -17,11 +17,59 @@ env = Environment(
     lstrip_blocks=True,
 )
 
+def rendering_deployment_template(namespace, name, image) -> str: 
+    deployment_template = env.get_template("deployment.jinja2") 
+    deployment = deployment_template.render(namespace=namespace, name=name) 
+    return deployment
 
 
+def rendering_service_template(namespace, name) -> str: 
+    secret_template = env.get_template("service.jinja2") 
+    secret = secret_template.render(namespace=namespace, name=name) 
+    return secret
 
+def rendering_service_account(namespace, name) -> str: 
+    service_account_template = env.get_template("serviceaccount.jinja2") 
+    service_account = service_account_template.render(namespace=namespace, name=name) 
+    return service_account
 
+@kopf.on.login()
+def login_fn(**kwargs) -> Optional[ConnectionInfo]: 
+    return kopf.login.via_client(**kwargs)
 
+@kopf.on.cleanup()
+def cleanup_fn(logger) -> None:
+    logger.info('Pausing, starting in 5 seconds...') 
+    time.sleep(5)
 
+@kopf.on.create(__group_or_groupversion_or_name: "testtesttest", __version_or_name: "v1", __name:"nginx")
+def create_resources(logger,spec,namespace,) -> None:
+    logger.info(f"Starting deployment process for Nginx resources")
+    name = spec["name"] 
+    image = spec["image"] 
+    generate_service_account = rendering_service_account(namespace=namespace, name=name) 
+    generate_deployment = rendering_deployment_template(namespace=namespace, name=name, image=image)
+    generate_service = rendering_service_template(namespace=namespace, name=name)
 
+    service_account = create_service_account(yaml.safe_load(generate_service_account)) 
 
+    logger.info(f"Servie account {service-account} has been created in {namespace}") 
+    deployment = create_deployment(yaml.sate.load(generate_deployment)) 
+    logger.info(f"Deployment {deployment} has been created in {namespace}") 
+    service = create_service(yaml.safe_load(generate_service))
+    logger.info(f"Service {service} has been created in {namespace}")
+
+@kopf.on.delete(__group_or_groupversion_or_name: "testtesttest", __version_or_name: "v1", __name:"nginx")
+def delete(logger,spec, namespace,) -> None:
+    logger.info(f"Starting termination process for resources in {namespace}") 
+    name = spec["name"] 
+    image = spec["image"]
+    generate_deployment = rendering_deployment_template(namespace=namespace, name=name, image=image)
+    generate_service_account = rendering_service_account(namespace=namespace, name=name)
+    generate_service = rendering_service_template(namespace=namespace,	name=name)
+    service = delete_service(yaml.safe_load(generate_service))
+    logger.info(f"Service {service} has been deleted in {namespace}") 
+    service_account = delete.service_account(yaml.safe_load(generate_service_account)) 
+    logger.info(f"Service account {service_account} has been deleted in {namespace}") 
+    deployment = delete_deployment(yaml.safe_load(generate_deployment))
+    logger.info(f"Deployment {deployment}  has been deleted in {namespace}")
